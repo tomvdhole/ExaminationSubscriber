ccf67ed2664dc4fa40371229b139c5d3b1531109
Taisho Lede <taisho.lede@gmail.com>
2016-03-20 14:08:18 +0100

    Afternoon build

--- Subscription/models.py
+++ Subscription/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-# Create your models here.
 
 BELT_TO_GRADE = (("0e Kyu", "Witte gordel"), ("9e Kyu", "Witte gordel met zwart streepje"), ("9e Kyu", "Rode gordel"), ("8e Kyu", "Gele gordel"), ("7e Kyu", "Oranje gordel"), ("6e Kyu", "Groene gordel"), ("5e Kyu", "Blauwe gordel"), ("4e Kyu", "Blauwe gordel met wit streepje"), ("3e Kyu", "Bruine gordel"), ("2e Kyu", "Bruine gordel met wit streepje"), ("1e Kyu", "Bruine gordel met twee witte streepjes"), )
 RED_RIBBONS = ((0, "0"), (1, "1"), (2, "2"),)
@@ -28,5 +27,23 @@ class Participant(models.Model):
     age = models.CharField(max_length=2, validators= [RegexValidator(r"^[0-9]{1,2}$", "Age must consists of a numerical value of maximum 2 figures!")])
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__save_data()
+
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+    def __save_data(self):
+        self.__saved = False
+        if self.age == 7 and (self.number_of_red_ribbons == 0 or self.number_of_red_ribbons == 1):
+            if self.number_of_red_ribbons == 0:
+                self.category = Category.objects.get(type_of_competitors='Kids', examination_type='Form')
+                self.__saved = True
+            if self.number_of_red_ribbons == 1:
+                self.category = Category.objects.get(type_of_competitors='Kids', examination_type='Rhytm')
+                self.__saved = True
+
+    @property
+    def saved(self):
+        return self.__saved
--- Subscription/views.py
+++ Subscription/views.py
@@ -16,26 +16,8 @@ def subscribe(request):
     else:
         form = ParticipantForm(request.POST)
         if form.is_valid():
-            saved = __save_data(form)
-            #define_category
-            return redirect('succes', saved=saved)
+            participant = form.save(commit=False)
+            participant.save()
+            return redirect('succes', saved=participant.saved)
         else:
             return render(request, 'Subscription/subscribe.html', {'form' : form})
-
-
-#Private methods
-def __save_data(form):
-    age = form.cleaned_data['age']
-    grade = form.cleaned_data['grade']
-    number_of_red_ribbons = form.cleaned_data['number_of_red_ribbons']
-    saved = False
-
-    if age == 7 and (number_of_red_ribbons == 0 or number_of_red_ribbons == 1):
-        if number_of_red_ribbons == 0:
-            category = Category.objects.get(type_of_competitors='Kids', examination_type='Form')
-            saved = True
-        if number_of_red_ribbons == 1:
-            category = Category.objects.get(type_of_competitors='Kids', examination_type='Rhytm')
-            saved = True
-
-    return saved
