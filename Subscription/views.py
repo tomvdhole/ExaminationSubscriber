dcbdeedab497730e388cf46592ecb29ec0d60d54
Taisho Lede <taisho.lede@gmail.com>
2016-03-20 20:56:48 +0100

    Cleanup van test code

--- Subscription/views.py
+++ Subscription/views.py
@@ -11,26 +11,13 @@ def succes(request, saved):
 
 def subscribe(request):
     if request.method == 'GET':
-        print("get")
         form = ParticipantForm()
         return render(request, 'Subscription/subscribe.html', {'form' : form})
     else:
-        print("post")
         form = ParticipantForm(request.POST)
-        print("form created")
         if form.is_valid():
             participant = form.save()
-            #participant.save_data()
             participant.save()
-            print("object created")
-            print("in view")
-            print(participant.age)
-            print(participant.number_of_red_ribbons)
-            print(participant.category)
-            print(participant.saved)
-            print("uit view")
-
-
             return redirect('succes', saved=participant.saved)
         else:
             return render(request, 'Subscription/subscribe.html', {'form' : form})
