02e48317b43c8419ae516fe85ea55bd63e7a7857
Taisho Lede <taisho.lede@gmail.com>
2016-03-22 19:42:04 +0100

    verbetering code

--- Subscription/models.py
+++ Subscription/models.py
@@ -3,15 +3,15 @@ from django.core.validators import RegexValidator
 
 BELT_TO_GRADE = (("0e Kyu", "Witte gordel"), ("9e Kyu", "Witte gordel met zwart streepje"), ("9e Kyu", "Rode gordel"), ("8e Kyu", "Gele gordel"), ("7e Kyu", "Oranje gordel"), ("6e Kyu", "Groene gordel"), ("5e Kyu", "Blauwe gordel"), ("4e Kyu", "Blauwe gordel met wit streepje"), ("3e Kyu", "Bruine gordel"), ("2e Kyu", "Bruine gordel met wit streepje"), ("1e Kyu", "Bruine gordel met twee witte streepjes"), )
 RED_RIBBONS = ((0, "0"), (1, "1"), (2, "2"),)
-TYPE_OF_COMPETITORS = (("Beginners", "Beginners"), ("Advanced", "Gevorderden"), ("Kids", "Kinderen"), ("Youth", "Jeugd"),)
+TYPE_OF_PARTICIPANTS = (("Beginners", "Beginners"), ("Advanced", "Gevorderden"), ("Kids", "Kinderen"), ("Youth", "Jeugd"),)
 EXAMINATION_TYPE = (("Form", "Vorm"), ("Rhytm", "Ritme"), ("Official", "Officieel"),)
 
 class Category(models.Model):
-    type_of_competitors = models.CharField(max_length=20, choices=TYPE_OF_COMPETITORS)
+    type_of_participants = models.CharField(max_length=20, choices=TYPE_OF_PARTICIPANTS)
     examination_type = models.CharField(max_length=10, choices= EXAMINATION_TYPE)
 
     def __str__(self):
-        return self.type_of_competitors + " " + self.examination_type
+        return self.type_of_participants + " " + self.examination_type
 
 class Participant(models.Model):
     last_name = models.CharField(max_length=30)
@@ -35,12 +35,14 @@ class Participant(models.Model):
 
     #private method
     def __save_data(self):
-        if self.age == '7' and (self.number_of_red_ribbons == 0 or self.number_of_red_ribbons == 1):
+        self.age = int(self.age)
+
+        if self.age == 7 and (self.number_of_red_ribbons == 0 or self.number_of_red_ribbons == 1):
             if self.number_of_red_ribbons == 0:
-                self.category = Category.objects.get(type_of_competitors='Kids', examination_type='Form')
+                self.category = Category.objects.get(type_of_participants='Kids', examination_type='Form')
                 self.__saved = True
             if self.number_of_red_ribbons == 1:
-                self.category = Category.objects.get(type_of_competitors='Kids', examination_type='Rhytm')
+                self.category = Category.objects.get(type_of_participants='Kids', examination_type='Rhytm')
                 self.__saved = True
 
     @property
