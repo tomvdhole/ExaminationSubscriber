e96794a19086e272095bb5f2a8228f448e8ba279
Taisho Lede <taisho.lede@gmail.com>
2016-04-02 20:30:40 +0200

    Business logica toegevoegd

--- Subscription/forms.py
+++ Subscription/forms.py
@@ -1,18 +1,21 @@
 from django import forms
+from django.forms import extras
 from .models import Participant
 
 class ParticipantForm(forms.ModelForm):
-    gsm = forms.RegexField(regex=r"^04[0-9]{8,8}$", error_messages={'invalid':'GSM number must consists of 10 numbers and it must start with a 04!'})
-    license_number = forms.RegexField(regex=r"^[0-9]{1,6}$", error_messages={'invalid':'License number may only consists of maximum 6 figures!'})
-    age = forms.RegexField(regex=r"^[0-9]{1,2}$", error_messages={'invalid': 'Age must consists of a numerical value of maximum 2 figures!'})
+    gsm = forms.RegexField(regex=r"^04[0-9]{8,8}$", error_messages={'invalid':'GSM nummer moet bestaan uit 10 nummers en het moet starten met 04!'}, label="Gsm")
+    license_number = forms.RegexField(regex=r"^[0-9]{1,6}$", error_messages={'invalid':'Licentie nummer mag enkel bestaan uit maximum 6 cijfers!'}, label="Licentienummer")
+    age = forms.RegexField(regex=r"^[0-9]{1,2}$", error_messages={'invalid': 'Leeftijd moet bestaan uit maximum 2 cijfers!'}, label="Leeftijd")
+    date_till_license_is_valid= forms.DateField(widget = extras.SelectDateWidget(), label="Licentie geldigheids datum")
 
     class Meta:
         model = Participant
         fields = ['last_name',
                   'first_name',
-                  'gsm',
-                  'email',
                   'license_number',
-                  "grade",
-                  "number_of_red_ribbons",
-                  "age"]
+                  'date_till_license_is_valid',
+                  'grade',
+                  'number_of_red_ribbons',
+                  'age',
+                  'gsm',
+                  'email']
--- Subscription/models.py
+++ Subscription/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-BELT_TO_GRADE = (("0e Kyu", "Witte gordel"), ("9e Kyu", "Witte gordel met zwart streepje"), ("9e Kyu", "Rode gordel"), ("8e Kyu", "Gele gordel"), ("7e Kyu", "Oranje gordel"), ("6e Kyu", "Groene gordel"), ("5e Kyu", "Blauwe gordel"), ("4e Kyu", "Blauwe gordel met wit streepje"), ("3e Kyu", "Bruine gordel"), ("2e Kyu", "Bruine gordel met wit streepje"), ("1e Kyu", "Bruine gordel met twee witte streepjes"), )
+BELT_TO_GRADE = (("0e Kyu", "Witte gordel"), ("9e Kyu", "Witte gordel met zwart streepje"), ("9e Kyu", "Rode gordel"), ("8e Kyu", "Gele gordel"), ("7e Kyu", "Oranje gordel"), ("6e Kyu", "Groene gordel"), ("5e Kyu", "Blauwe gordel"), ("4e Kyu", "Blauwe gordel met wit streepje"), ("3e Kyu", "Bruine gordel"), ("2e Kyu", "Bruine gordel met wit streepje"),)
 RED_RIBBONS = ((0, "0"), (1, "1"), (2, "2"),)
 TYPE_OF_PARTICIPANTS = (("Beginners", "Beginners"), ("Advanced", "Gevorderden"), ("Kids", "Kinderen"), ("Youth", "Jeugd"),)
 EXAMINATION_TYPE = (("Form", "Vorm"), ("Rhytm", "Ritme"), ("Official", "Officieel"),)
@@ -14,14 +14,15 @@ class Category(models.Model):
         return self.type_of_participants + " " + self.examination_type
 
 class Participant(models.Model):
-    last_name = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    gsm = models.CharField(max_length=10, validators=[RegexValidator(r"^04[0-9]{8}$", "GSM number must consists of 10 numbers and it must start with a 0!")])
-    email = models.EmailField()
-    license_number = models.CharField(max_length=6, validators=[RegexValidator(r"^[0-9]{1,6}$", "License number may only consists of maximum 6 figures!")])
-    grade = models.CharField(max_length=6, choices=BELT_TO_GRADE)
-    number_of_red_ribbons = models.PositiveSmallIntegerField(choices=RED_RIBBONS)
-    age = models.CharField(max_length=2, validators= [RegexValidator(r"^[0-9]{1,2}$", "Age must consists of a numerical value of maximum 2 figures!")])
+    last_name = models.CharField(max_length=30, verbose_name="Naam")
+    first_name = models.CharField(max_length=30, verbose_name="Voornaam")
+    gsm = models.CharField(max_length=10, validators=[RegexValidator(r"^04[0-9]{8}$", "GSM nummer moet bestaan uit 10 nummers en het moet starten met 04!")], verbose_name="Gsm")
+    email = models.EmailField(verbose_name="Email")
+    license_number = models.CharField(max_length=6, validators=[RegexValidator(r"^[0-9]{1,6}$", "Licentie nummer mag enkel bestaan uit maximum 6 cijfers!")], verbose_name="Licentienummer")
+    date_till_license_is_valid = models.DateField(verbose_name="Licentie geldigheids datum")
+    grade = models.CharField(max_length=6, choices=BELT_TO_GRADE, verbose_name="Graad")
+    number_of_red_ribbons = models.PositiveSmallIntegerField(choices=RED_RIBBONS, verbose_name="Aantal rode lintjes")
+    age = models.CharField(max_length=2, validators= [RegexValidator(r"^[0-9]{1,2}$", "Leeftijd moet bestaan uit maximum 2 cijfers!")], verbose_name="Leeftijd")
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self):
@@ -44,10 +45,49 @@ class Participant(models.Model):
             if self.number_of_red_ribbons == 1:
                 self.category = Category.objects.get(type_of_participants='Kids', examination_type='Rhytm')
                 self.__saved = True
-        #officieel
-        if (self.age >= 8 and self.age < 11) and (grade == "0e Kyu" or grade == "9e Kyu" or grade == "8e Kyu" or grade == "7e Kyu" or grade == "6e Kyu"):
+
+        if (self.age >= 8 and self.age < 11) and (self.grade == "0e Kyu" or self.grade == "9e Kyu" or self.grade == "8e Kyu" or self.grade == "7e Kyu" or self.grade == "6e Kyu"):
             if self.number_of_red_ribbons == 0:
                 self.category = Category.objects.get(type_of_participants='Kids', examination_type='Form')
+                self.__saved = True
+            if self.number_of_red_ribbons == 1:
+                self.category = Category.objects.get(type_of_participants='Kids', examination_type='Rhytm')
+                self.__saved = True
+            if self.number_of_red_ribbons == 2:
+                self.category = Category.objects.get(type_of_participants='Kids', examination_type='Official')
+                self.__saved = True
+
+        if (self.age >= 11 and self.age < 14) and (self.grade == "0e Kyu" or self.grade == "9e Kyu" or self.grade == "8e Kyu" or self.grade == "7e Kyu" or self.grade == "6e Kyu"):
+            if self.number_of_red_ribbons == 0:
+                self.Category = Category.objects.get(type_of_participants='Youth', examination_type='Form')
+                self.__saved = True
+            if self.number_of_red_ribbons == 1:
+                self.category = Category.objects.get(type_of_participants='Youth', examination_type='Rhytm')
+                self.__saved = True
+            if self.number_of_red_ribbons == 2:
+                self.category = Category.objects.get(type_of_participants='Youth', examination_type='Official')
+                self.__saved = True
+
+        if self.age >= 14 and (self.grade == "0e Kyu" or self.grade == "9e Kyu" or self.grade == "8e Kyu" or self.grade == "7e Kyu" or self.grade == "6e Kyu"):
+            if self.number_of_red_ribbons == 0:
+                self.Category = Category.objects.get(type_of_participants='Beginners', examination_type='Form')
+                self.__saved = True
+            if self.number_of_red_ribbons == 1:
+                self.category = Category.objects.get(type_of_participants='Beginners', examination_type='Rhytm')
+                self.__saved = True
+            if self.number_of_red_ribbons == 2:
+                self.category = Category.objects.get(type_of_participants='Beginners', examination_type='Official')
+                self.__saved = True
+
+        if self.grade == "5e Kyu" or self.grade == "4e Kyu" or self.grade == "3e Kyu" or self.grade == "2e Kyu":
+            if self.number_of_red_ribbons == 0:
+                self.Category = Category.objects.get(type_of_participants='Advanced', examination_type='Form')
+                self.__saved = True
+            if self.number_of_red_ribbons == 1:
+                self.category = Category.objects.get(type_of_participants='Advanced', examination_type='Rhytm')
+                self.__saved = True
+            if self.number_of_red_ribbons == 2:
+                self.category = Category.objects.get(type_of_participants='Advanced', examination_type='Official')
                 self.__saved = True
 
     @property
